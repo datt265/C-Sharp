65996464917e9ed9dec17329659fe90b8850ffbc
Daniel Attard <45830142+datt265@users.noreply.github.com>
2019-04-17 06:16:47 +0100

    Unit Testing using MS Tests
    A simple console application using MS Tests and AxoCover

---
+++ UnitTests-SimpleExample/MSTests/ConsoleApplicationUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplicationUnitTests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void NegativeNumber()
+        {
+            Assert.AreEqual(10, new Math().Abs(-10));
+        }
+
+        [TestMethod]
+        public void Zero()
+        {
+            Assert.AreEqual(0, new Math().Abs(0));
+        }
+
+        [TestMethod]
+        public void PositiveNumber()
+        {
+            Assert.AreEqual(5, new Math().Abs(5));
+        }
+
+
+    }
+}
---
+++ UnitTests-SimpleExample/MSTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Math
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int Abs(int n)
+        {
+            return n < 0 ? -n : n;
+        }
+    }
+}
